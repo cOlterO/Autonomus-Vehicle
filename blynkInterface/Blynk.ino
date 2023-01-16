f3efc1733595c8b48f4515d20218447c03c71d0a
Benjamin-86 <67819136+Benjamin-86@users.noreply.github.com>
2023-01-16 14:10:12 +0100

    V1.1

--- blynkInterface/Blynk.ino
+++ blynkInterface/Blynk.ino
@@ -1,33 +1,25 @@
-/*
-  Blynk console for car control
+//Blynk auth credentials: 
+//Should be placed before headers
+#define BLYNK_TEMPLATE_ID           "TMPLBotx3_ZT"
+#define BLYNK_DEVICE_NAME           "Car controls"
+#define BLYNK_AUTH_TOKEN            "f1cGTooC6I4YG9aXzC_tDHCHaF_oC-zW"
 
-  NOTE:BlynkTimer provides SimpleTimer functionality:
-  http://playground.arduino.cc/Code/SimpleTimer
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
 
-*/
 
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WiFiServer.h>
-#include <WiFiUdp.h>
 #include <BlynkSimpleEsp32.h>
 #include "DHT.h"
 
-
-//Blynk auth details: 
-
-#define BLYNK_TEMPLATE_ID "TMPLBotx3_ZT"
-#define BLYNK_DEVICE_NAME "Car controls"
-#define BLYNK_AUTH_TOKEN  "f1cGTooC6I4YG9aXzC_tDHCHaF_oC-zW"
-
-#define BLYNK_PRINT Serial                        // Comment this out to disable prints and save space
-
-char auth[] = BLYNK_AUTH_TOKEN;                   
-char ssid[] = "Dontaskmypassword";                  //your SSID              
-char pass[] = "ben168001";                     // Password
+//Networking credentials
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "SSID";       //SHOULD BE CHANGED 
+char pass[] = "PASSWORD";   //SAME HER TOO 
 
 //Pinouts 
-#define LED_BUILTIN 32
+#define LED_BUILTIN 2
 #define tempPIN 33
 #define dhtPIN 25
 #define offsetValue 2  
@@ -35,38 +27,48 @@ char pass[] = "ben168001";                     // Password
 BlynkTimer timer;
 DHT dht(dhtPIN, DHT11); 
 
+// Updates every 1S
+void myTimerEvent(){
+  Blynk.virtualWrite(V0, digitalRead(LED_BUILTIN));               //(INT) inbuild Led state. 
+ // Blynk.virtualWrite(V1, xxx);                                    //(INT) Front Ultrasensor value on xxx
+  //Blynk.virtualWrite(V2, xxx);                                    //(INT) Right Ultrasensor value    
+  //Blynk.virtualWrite(V3, xxx);                                    //(INT) Left Ultrasenor value 
+  Blynk.virtualWrite(V4, tempData());                                    //(Float) Battery temp
+ //Blynk.virtualWrite(V5, xxx);                                    //(String) AI prediction 
+  //Blynk.virtualWrite(V6, xxx);                                  //Right motor LED if needed 
+ //Blynk.virtualWrite(V7, xxx);                                   //Left motor LED if needed 
+ Blynk.virtualWrite(V8,dht.readTemperature() );
+ 
+}
 
 void setup(){
-
+ 
   Serial.begin(9600);
 
   Blynk.begin(auth, ssid, pass);
-  timer.setInterval(1000L, blynkData);    //Setup blynkData to be called every 1S
 
-    dht.begin();
+  //Function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
 
+  dht.begin(); //DHT sensor maybe usless
 
 }
 
 void loop(){
-
-
-  digitalWrite(LED_BUILTIN, LOW); 
-
-  Blynk.run();                          // Init Blynk 
-  timer.run();                          // Initiates BlynkTimer
-
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+  digitalWrite(LED_BUILTIN, HIGH); 
 }
 
 
- //Function will be called every 1s and sends value to the blynk console. 
-void blynkData(){
- 
+float tempData(){
+  
   int inputValue = analogRead(tempPIN);            //Reads the value of potentiometer
   float milliVolt = inputValue * (3300.0 / 4096.0);   //Calculates the corressponding milli Voltage range from 0 - 5V
   float temp = milliVolt / 10;                   //Datasheet specifies that LM35 reads 10mv/°C and adds to the value before.
   temp = temp *offsetValue; 
 
+  /*Debuging thing for temp 
   Serial.println("......................");
   Serial.println(" "); 
   Serial.println("Reading: " + (String)inputValue); 
@@ -75,15 +77,7 @@ void blynkData(){
   Serial.println("Temp DHT: " + (String)dht.readTemperature());
   Serial.println(" "); 
   Serial.println("......................");
+  */
 
-
-  Blynk.virtualWrite(V0, digitalRead(LED_BUILTIN));               //(INT) inbuild Led state. 
- // Blynk.virtualWrite(V1, xxx);                                    //(INT) Front Ultrasensor value on xxx
-  //Blynk.virtualWrite(V2, xxx);                                    //(INT) Right Ultrasensor value    
-  //Blynk.virtualWrite(V3, xxx);                                    //(INT) Left Ultrasenor value 
-  Blynk.virtualWrite(V4, temp);                                    //(Float) Battery temp
- //Blynk.virtualWrite(V5, xxx);                                    //(String) AI prediction 
-  //Blynk.virtualWrite(V6, xxx);                                  //Right motor LED if needed 
- //Blynk.virtualWrite(V7, xxx);                                   //Left motor LED if needed 
-
+  return temp; 
 }
