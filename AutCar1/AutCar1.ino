7faa962cee2b2ff569743aef04ea6b5afa30ce0c
Brendan van de Venter <s204770@student.dtu.dk>
2023-01-17 11:59:31 +0100

    Basic IOT functions added to car

--- AutCar1/AutCar1.ino
+++ AutCar1/AutCar1.ino
@@ -1,8 +1,16 @@
-
-#include <NewPing.h>
+/*
+  ___        _                                                _____            
+ / _ \      | |                                              /  __ \           
+/ /_\ \_   _| |_ ___  _ __   ___  _ __ ___   ___  _   _ ___  | /  \/ __ _ _ __ 
+|  _  | | | | __/ _ \| '_ \ / _ \| '_ ` _ \ / _ \| | | / __| | |    / _` | '__|
+| | | | |_| | || (_) | | | | (_) | | | | | | (_) | |_| \__ \ | \__/\ (_| | |   
+\_| |_/\__,_|\__\___/|_| |_|\___/|_| |_| |_|\___/ \__,_|___/  \____/\__,_|_|   
+                                                                               
+*/
+#include <NewPing.h>  // Sonar library
 
 #define SONAR_NUM 3       // Number of sensors.
-#define MAX_DISTANCE 100  // Maximum distance (in cm) to ping.
+#define MAX_DISTANCE 250  // Maximum distance (in cm) to ping.
 
 NewPing sonar[SONAR_NUM] = {      // Sensor object array.
   NewPing(33, 35, MAX_DISTANCE),  // Each sensor's trigger pin, echo pin, and max distance to ping.
@@ -32,18 +40,19 @@ const int in4 = 27;
 
 
 
-//buzzer stuff
+//buzzer musical pitches
 #include "pitches.h"
 
+// Buzzer pin
 const int BUZZER_PIN = 4;
-
+// Nokia melody pitches
 int melody[] = {
   NOTE_E5, NOTE_D5, NOTE_FS4, NOTE_GS4,
   NOTE_CS5, NOTE_B4, NOTE_D4, NOTE_E4,
   NOTE_B4, NOTE_A4, NOTE_CS4, NOTE_E4,
   NOTE_A4
 };
-
+// Nokia melody duration lenght of pitches
 int durations[] = {
   8, 8, 4, 4,
   8, 8, 4, 4,
@@ -51,18 +60,65 @@ int durations[] = {
   2
 };
 
+int durations2[] = {
+  4, 2, 8, 8,
+  4, 4, 3, 6,
+  5, 2, 3, 3,
+  2
+};
 
-int d1 = 0;
-int d2 = 0;
-int d3 = 0;
+// Variables for distance of the ultrasound sensors
+int d1 = 0;  // Front
+int d2 = 0;  // Left
+int d3 = 0;  // Right
+
+// Variable where prediction string will be stored
+String prediction = "";
+String lastPred = "";
+
+//Blynk auth credentials:
+//Should be placed before headers
+#define BLYNK_TEMPLATE_ID "TMPLBotx3_ZT"
+#define BLYNK_DEVICE_NAME "Car controls"
+#define BLYNK_AUTH_TOKEN "f1cGTooC6I4YG9aXzC_tDHCHaF_oC-zW"
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+
+//Networking credentials
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Whax";      //SHOULD BE CHANGED
+char pass[] = "1234567a";  //SAME HER TOO
+
+//Pinouts
+// #define LED_BUILTIN 2
+#define tempPIN 33
+#define offsetValue 2
+
+BlynkTimer timer;
+
+
+// Updates every 1S
+void myTimerEvent() {
+  // Blynk.virtualWrite(V0, digitalRead(LED_BUILTIN));               //(INT) inbuild Led state.
+  Blynk.virtualWrite(V1, d1);          //(INT) Front Ultrasensor value on xxx
+  Blynk.virtualWrite(V2, d2);          //(INT) Left Ultrasensor value
+  Blynk.virtualWrite(V3, d3);          //(INT) Right Ultrasenor value
+  Blynk.virtualWrite(V4, tempData());  //(Float) Battery temp
+  Blynk.virtualWrite(V5, lastPred);  //(String) AI prediction
+                                       //Blynk.virtualWrite(V6, xxx);                                  //Right motor LED if needed
+  //Blynk.virtualWrite(V7, xxx);                                   //Left motor LED if needed
+}
 
 
 
 const byte LED_BUILTIN = 32;
-
-String prediction = "";
-
-//Initialize distance for Ultrasound sensors
 
 
 
@@ -79,11 +135,13 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-
+  // pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 
-  // attachInterrupt(digitalPinToInterrupt(sensor_pin), movement_detection, RISING);
+
+  Blynk.begin(auth, ssid, pass);
+  //Function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
 
   // Turn off motors - Initial state(All LOW)
   initialStateMotors();
@@ -93,31 +151,53 @@ void setup() {
 }
 
 void loop() {
+  Blynk.run();
+  timer.run();  // Initiates BlynkTimer
 
-  takeMeasurements();
+  dMeasure();
+  //fetches prediction
+  serial2Serial();
 
-  //checks car isnt too close to side walls
-  while ((d2 > 5) && (d3 > 5)) {
-    takeMeasurements();
-    Serial.println("HERE");
+  //forward if car isnt too close to wall
+  if (d1 > 40) {
+    forward();
+    dMeasure();
+  } else {
+    Serial.println("ENTERS ELSE");
+    //Stops car
+    initialStateMotors();
+    //performs movement based on prediction
+    movement(prediction);
 
-    //car stops in front of wall
-    if (d1 > 20) {
-      takeMeasurements();
-      Serial.println("FORWARD!");
-      forward();
+    //stores latest prediction so that it doesnt get cleared(For IOT)
+    lastPred = prediction;
 
-    } else {
-      initialStateMotors();
-      Serial.println("Reading sign");
-      serial2Serial();
-      delay(3000);
-      movement(prediction);
-      prediction = "";
-      Serial2.end();
-      Serial2.begin(115200);
-    }
+    //resets buffer
+    prediction = "";
+    Serial2.end();
+    Serial2.begin(115200);
   }
+}
+
+float tempData() {
+
+  int inputValue = analogRead(tempPIN);              //Reads the value of potentiometer
+  float milliVolt = inputValue * (3300.0 / 4096.0);  //Calculates the corressponding milli Voltage range from 0 - 5V
+  float temp = milliVolt / 10;                       //Datasheet specifies that LM35 reads 10mv/°C and adds to the value before.
+  temp = temp * offsetValue;
+
+  /*Debuging thing for temp 
+  Serial.println("......................");
+  Serial.println(" "); 
+  Serial.println("Reading: " + (String)inputValue); 
+  Serial.println("volt: " + (String)milliVolt); 
+  Serial.println("Temp: " + (String)temp); 
+  Serial.println("Temp DHT: " + (String)dht.readTemperature());
+  Serial.println(" "); 
+  Serial.println("......................");
+  */
+
+  return temp;
 }
 
 void nokia() {
@@ -139,7 +219,9 @@ void nokia() {
   }
 }
 
-void takeMeasurements() {
+
+
+void dMeasure() {
   d1 = sonar[0].ping_cm();
   delay(30);
   d2 = sonar[1].ping_cm();
@@ -164,30 +246,39 @@ void serial2Serial() {
     prediction += data;
   }
   if (prediction.length() > 1) {
-
-    Serial.println(prediction.substring(1));
-    serial
-  }else{
+    prediction = prediction.substring(1);
+  } else {
     Serial.println("NoPrediction");
   }
 }
 
+//switch case for predictions
 void movement(String prediction) {
 
   switch (tolower(prediction[0])) {
 
     case 'u':
       Serial.println("UNKNOWNWNW");
+      break;
 
     case 's':
       nokia();
+      delay(5000);
+      break;
 
     case 'r':
-      right1motor(500);
+      right1motor(640);
       Serial.println("Turning Right");
+      delay(5000);
+      break;
 
     case 'l':
-      left1motor(500);
+      left1motor(640);
+      delay(5000);
+      break;
+
+    default:
+      break;
   }
 }
 
@@ -212,7 +303,7 @@ void bForward() {
 
 void forward() {
   analogWrite(enA, 134);
-  analogWrite(enB, 140);
+  analogWrite(enB, 142);
   aForward();
   bForward();
 }
@@ -236,8 +327,8 @@ void bBackward() {
 }
 
 void left1motor(int time) {  // makes a left turn using 1 motor lasting the time you specified
-  analogWrite(enA, 140);
-  analogWrite(enB, 140);
+  analogWrite(enA, 160);
+  analogWrite(enB, 160);
 
   //left motor OFF
   digitalWrite(in1, LOW);
@@ -252,8 +343,8 @@ void left1motor(int time) {  // makes a left turn using 1 motor lasting the time
 }
 
 void right1motor(int time) {
-  analogWrite(enA, 140);
-  analogWrite(enB, 140);
+  analogWrite(enA, 160);
+  analogWrite(enB, 160);
 
   //right motor OFF
   digitalWrite(in3, LOW);
